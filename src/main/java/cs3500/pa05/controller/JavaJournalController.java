c8d43624b499799cec39158a150c86bca6bec652
hmcclew <122323895+hmcclew@users.noreply.github.com>
2023-06-13 15:30:03 -0400

    UML

--- src/main/java/cs3500/pa05/controller/JavaJournalController.java
+++ src/main/java/cs3500/pa05/controller/JavaJournalController.java
@@ -1,10 +1,52 @@
 package cs3500.pa05.controller;
 
 import cs3500.pa05.model.JavaJournalModel;
+import cs3500.pa05.model.week.Week;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.SplitPane;
 
 public class JavaJournalController implements Controller {
 
   private JavaJournalModel model;
+
+  private Week currentWeek;
+
+  @FXML
+  private Button openFile;
+
+  @FXML
+  private Button saveToFile;
+
+  @FXML
+  private Button showSideBar;
+
+  @FXML
+  private Button acceptWarning;
+
+  @FXML
+  private Button createAssignment;
+
+  @FXML
+  private ChoiceBox taskOrEvent;
+
+  @FXML
+  private Button completeCreatingAssignment;
+
+  @FXML
+  private Button markCompleteTask;
+
+  @FXML
+  private Button selectCategory;
+
+  @FXML
+  private Button addNewCategory;
+  @FXML
+  private ChoiceBox categories;
+
+  @FXML
+  private SplitPane sideBar;
 
   @Override
   public void run() throws IllegalStateException {
