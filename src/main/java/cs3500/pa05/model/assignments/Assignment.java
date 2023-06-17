d4baed90426c8f0da09322d4e27e42035df9b8e8
hmcclew <122323895+hmcclew@users.noreply.github.com>
2023-06-17 15:01:45 -0400

    style

--- src/main/java/cs3500/pa05/model/assignments/Assignment.java
+++ src/main/java/cs3500/pa05/model/assignments/Assignment.java
@@ -4,15 +4,46 @@ package cs3500.pa05.model.assignments;
  * interface for an assignment for a week
  */
 public interface Assignment {
+
+  /**
+   * getter method for the assignment day
+   *
+   * @return the assignment day
+   */
   String getDay();
 
+  /**
+   * getter method for the assignment name
+   *
+   * @return the assignment name
+   */
   String getName();
 
+  /**
+   * gets the description for this assignment
+   *
+   * @return the appropriate description
+   */
   String getDescription();
 
+  /**
+   * gets the category for this assignment
+   *
+   * @return the appropriate category
+   */
   String getCategory();
 
+  /**
+   * returns the event as a json string
+   *
+   * @return the formatted json string
+   */
   String toJsonFormat();
 
+  /**
+   * sets the category of this assignment
+   *
+   * @param category the category to be set
+   */
   void setCategory(String category);
 }
